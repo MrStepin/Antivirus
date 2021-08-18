91b951576f26161d6fd9a4b4d59f9f8cfc00ce37
Стёпин Станислав Александрович <stanislavalex@mail.ru>
2021-08-18 23:02:07 +0300

    .

--- Antivirus/Program.cs
+++ Antivirus/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,37 @@ namespace Antivirus
     {
         static void Main(string[] args)
         {
+            ScanFolder scanFolder = new ScanFolder();
+
+            string pathToFile = "C:\\Users\\Stas\\Desktop\\test";
+
+            string nameOfFile = "C:\\Users\\Stas\\Desktop\\test\\virus.txt";
+
+            scanFolder.NotifyWithFileName += DeleteFile;
+
+            scanFolder.NotifyWithFileName += WriteOnConsole;
+
+            scanFolder.Notify += SendEmail;
+
+            string [] filesFromFolder = scanFolder.GetFiles(pathToFile);
+
+            scanFolder.findFile(filesFromFolder, nameOfFile);
+
+            Console.ReadLine();
+        }
+        private static void DeleteFile(string nameOfFile)
+        {
+            File.Delete(nameOfFile);
+        }
+
+        private static void WriteOnConsole(string nameOfFile)
+        {
+            Console.WriteLine($"Found Virus: {nameOfFile}");
+        }
+
+        private static void SendEmail()
+        {
+            Console.WriteLine("Notification sent to email.");
         }
     }
 }
---
+++ Antivirus/ScanFolder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antivirus
+{
+    public class ScanFolder
+    {
+        public delegate void FileHandler();
+        public delegate void FileWithFileNameHandler(string fileName);
+        public event FileHandler Notify;
+        public event FileWithFileNameHandler NotifyWithFileName;
+
+        public string[] GetFiles (string directoryPath)
+        {
+            return Directory.GetFiles(directoryPath);
+        }
+
+        public void findFile(string[] files, string filePathAndName)
+        {
+            foreach (string fileFromArray in files)
+                if (filePathAndName == fileFromArray)
+                {
+                    Notify?.Invoke();
+                    NotifyWithFileName?.Invoke(filePathAndName);
+                }
+        }
+    }
+}
